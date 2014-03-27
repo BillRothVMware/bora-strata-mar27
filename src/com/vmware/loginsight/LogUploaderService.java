b27dc2d51efb62d6298976fcff965550e69ce8a3
Jeremy Olmsted-Thompson <jolmstedthomps@vmware.com>
2014-03-27 11:34:22 -0700

    First commit

---
+++ src/com/vmware/loginsight/LogUploaderService.java
@@ -0,0 +1,19 @@
+package com.vmware.loginsight;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class LogUploaderService extends IntentService {
+
+	public LogUploaderService(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onHandleIntent(Intent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
